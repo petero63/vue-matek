25e8fb11c58d4b356d80e7895883b36944627953
petero63 <peter.otto@gmail.com>
2018-10-14 23:46:51 +0200

    1114

--- src/components/PageFormula.vue
+++ src/components/PageFormula.vue
@@ -1,16 +1,27 @@
 <template>
-	<div class="hello" >
+	<div>
+	<div class="d-inline-block  bg-success" >xxxx </div>
+	<div class="d-inline-block  bg-warning" >yyy </div>
+	<div class="d-inline-block  bg-success" >zzz </div>
+	<h1 class="mathFormula">Pelda</h1>
+	<div class="alert alert-success"  v-html=example.pageContent ></div>
 		<button type="button" class="btn btn-danger" v-on:click="showFormula(0)">Rejts el</button>
 		<button type="button" class="btn btn-success" v-on:click="showFormula(1)">Mutasd</button>
 		<br>
-		<button type="button" class="btn btn-primary" v-on:click="isNight=!isNight">Rejts el 2</button>
+<div class="mathFormula d-inline-block ">
 		<div v-bind:class="(formulaClass)">
-			<div v-bind:class="(formulaClass)" v-for="(item, index) in steps" v-html=item.formula v-on:click="otto(index+1)" v-bind:id="index"></div>
+			<div 
+			v-bind:class=item.class 
+			v-for="(item, index) in steps" 
+			v-html=item.formula v-on:click="nextStep(index)" 
+			v-bind:id="index">
+			</div>
 			<div>
 			</div>
 			<hr>
 			<h3>Ez a json az AIP bol</h3>
 		</div>
+</div>
 
 	</div>
 
@@ -25,10 +36,10 @@ export default {
 	},
 	data () {
 		return {
+			example:"xxx",
 			isNight:true,
 			isActive:true,
-			formulaClass:"showFormula",
-
+			formulaClass:"showFormula ",
 			steps:[],
 		}
 	},
@@ -41,28 +52,39 @@ export default {
 			var steps=[];
 
 			const data = response.data;
-			const steps = [];
+			//const steps = [];
 			for (let key in data) { 
 				const step = data[key]; 
 				step.id = key; 
 				step.formula=step.formula.replace(/\\/g, '');
+				step.class = "hideFormula"; 
 				steps.push(step);
-				//console.log(step.formula);
 			}
+			steps[0].class="showFormula";
 
-			//console.log(steps);
+//			console.log(steps);
 			this.steps=steps;
+
+
 		}
 		);
 
-		//var a={"name":"Roland","korte":145};
-		var a=this.infoajaxfull;
 		//axios .get('http://localhost:3000/getformula/1/110') .then(response => (this.infoajaxfull = response));
+		axios .get('http://localhost:3000/getexample/1/hu') .then(response => {
+			const data = response.data;
+
+				data.pageContent=data.pageContent.replace(/\\/g, '');
+			console.log(data);
+			this.example=data;
+		}
+		
+		);
 	},
 	methods: {
 		goHome () {var a="Rám kattintottál";alert (a);},
 		showFormula (n) {
-			//var a="Otto allert";alert (n);
+
+	//		var a="Otto allert";alert (n);
 			if (n==0) {this.formulaClass="hideFormula";}
 			if (n==1) {this.formulaClass="showFormula";}
 
@@ -74,11 +96,10 @@ export default {
 
 		},
 
-		otto (n) {
-//			alert(n);
-	//		if (n==1) {this.formulaClass="showFormula";}
+		nextStep (n) {
+			//alert(n);
 			var m=n+1;
-			document.getElementById(m).setAttribute("class", "hideFormula");
+			document.getElementById(m).setAttribute("class", "shoFormula d-inline-block");
 
 		}
 	}
@@ -87,6 +108,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.test { background-color:blue;  }
+.mathFormula { font-size: 20px; color:#333333;cursor: pointer;  }
 .showFormula { display: block; }
 .hideFormula { display: none; }
-</style>
+</style >
